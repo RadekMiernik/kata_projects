c86fd2832fcf855f8dcc61f8d8284c7693ee68df
Radoslaw Miernik <radoslaw.miernik@gmail.com>
2020-05-25 16:48:35 +0200

    final_version

--- Bank_OCR/solution.py
+++ Bank_OCR/solution.py
@@ -1,3 +1,6 @@
+# instruction of exercise
+# https://codingdojo.org/kata/BankOCR/#comments-from-those-who-are-working-on-this-kata
+
 from colorama import Fore, Style, Back
 
 # dictionary of possibles other numbers than original
@@ -54,14 +57,16 @@ def check_account(account_given: str):
         return '{}{}{}{}'.format(Fore.BLACK, Back.LIGHTYELLOW_EX, 'ILL', Style.RESET_ALL)
 
 
-def check_possibilities(account_nr_to_check: str):  # TODO: actualize the docstring (returns) and ? problems
+def check_possibilities(account_nr_to_check: str):
     """Checking other possibilities if scanned data is not valid (ERR or ILL).
 
     Arguments:
         account_nr_to_check (str): string version of account
 
     Returns:
-        lists: other valid possibilities of account number
+        str: 'Account nr: {}'.format(others[0]) - if there is only one possibility
+        str: 'AMB {}'.format(others) - if there are more than one possibility
+        str: 'No other possibilities' - if there is no possibility for other number
     """
     others = []
     if check_account(account_nr_to_check) == '{}{}{}{}'.format(Fore.BLACK, Back.LIGHTRED_EX, 'ERR', Style.RESET_ALL)\
@@ -77,31 +82,25 @@ def check_possibilities(account_nr_to_check: str):  # TODO: actualize the docstr
             while i < 10:
                 for char in account_nr_to_check:
                     for _ in '1234567890':
-                        try:
-                            if int(_) in dict_possible.get(int(char)):
-                                if i == 0:
-                                    other = _ + account_nr_to_check[i + 1:]
-                                    valid = check_account(other)
-                                    if valid == '{}{}{}{}'.format(Fore.GREEN, Back.LIGHTBLACK_EX,
-                                                                  'VALID', Style.RESET_ALL) and other not in others:
-                                        others.append(other)
-                                elif 1 <= i < 9:
-                                    other = account_nr_to_check[:i] + _ + account_nr_to_check[i + 1:]
-                                    valid = check_account(other)
-                                    if valid == '{}{}{}{}'.format(Fore.GREEN, Back.LIGHTBLACK_EX,
-                                                                  'VALID', Style.RESET_ALL) and other not in others:
-                                        others.append(other)
-                        except ValueError:  # TODO: tutaj trzeba dodać validację źle odczytanego znaku, który daje ?
-                            pass
+                        if int(_) in dict_possible.get(int(char)):
+                            if i == 0:
+                                other = _ + account_nr_to_check[i + 1:]
+                                valid = check_account(other)
+                                if valid == '{}{}{}{}'.format(Fore.GREEN, Back.LIGHTBLACK_EX,
+                                                              'VALID', Style.RESET_ALL) and other not in others:
+                                    others.append(other)
+                            elif 1 <= i < 9:
+                                other = account_nr_to_check[:i] + _ + account_nr_to_check[i + 1:]
+                                valid = check_account(other)
+                                if valid == '{}{}{}{}'.format(Fore.GREEN, Back.LIGHTBLACK_EX,
+                                                              'VALID', Style.RESET_ALL) and other not in others:
+                                    others.append(other)
                     i += 1
         else:
             for _ in list_of_others:
-                valid = check_account(_)
-                if valid == '{}{}{}{}'.format(Fore.GREEN, Back.LIGHTBLACK_EX, 'VALID', Style.RESET_ALL) \
+                if check_account(_) == '{}{}{}{}'.format(Fore.GREEN, Back.LIGHTBLACK_EX, 'VALID', Style.RESET_ALL) \
                         and _ not in others:
                     others.append(_)
-    else:
-        print('Here you have to add sth to deal with ?')
     if len(others) == 1:
         return 'Account nr: {}'.format(others[0])
     elif len(others) > 1:
@@ -119,8 +118,7 @@ def lines_on_numbers(given_example: str, valid_list_of_numbers=None):
                                       character
 
     Returns:
-        str: account number in readable simple form
-        list: invalid character as a list of characters to work in other fnction
+        tuple: str, list (str of account and list contains symbols from invalid character to work in other function)
     """
     given_account = reading(given_example)
     if valid_list_of_numbers is None:
@@ -163,29 +161,29 @@ def reading(given_example: str):
             pass
             i += 1
         else:
-            if 28 <= i <= 30 or 56 <= i <= 58 or 84 <= i <= 86:
+            if 28 <= i <= 30 or 56 <= i <= 58 or 84 <= i <= 86:  # first number
                 num_1.append(_)
-            elif 31 <= i <= 33 or 59 <= i <= 61 or 87 <= i <= 89:
+            elif 31 <= i <= 33 or 59 <= i <= 61 or 87 <= i <= 89:  # second number
                 num_2.append(_)
-            elif 34 <= i <= 36 or 62 <= i <= 64 or 90 <= i <= 92:
+            elif 34 <= i <= 36 or 62 <= i <= 64 or 90 <= i <= 92:  # third number
                 num_3.append(_)
-            elif 37 <= i <= 39 or 65 <= i <= 67 or 93 <= i <= 95:
+            elif 37 <= i <= 39 or 65 <= i <= 67 or 93 <= i <= 95:  # fourth number
                 num_4.append(_)
-            elif 40 <= i <= 42 or 68 <= i <= 70 or 96 <= i <= 98:
+            elif 40 <= i <= 42 or 68 <= i <= 70 or 96 <= i <= 98:  # fifth number
                 num_5.append(_)
-            elif 43 <= i <= 45 or 71 <= i <= 73 or 99 <= i <= 101:
+            elif 43 <= i <= 45 or 71 <= i <= 73 or 99 <= i <= 101:  # sixth number
                 num_6.append(_)
-            elif 46 <= i <= 48 or 74 <= i <= 76 or 102 <= i <= 104:
+            elif 46 <= i <= 48 or 74 <= i <= 76 or 102 <= i <= 104:  # seventh number
                 num_7.append(_)
-            elif 49 <= i <= 51 or 77 <= i <= 79 or 105 <= i <= 107:
+            elif 49 <= i <= 51 or 77 <= i <= 79 or 105 <= i <= 107:  # eight number
                 num_8.append(_)
-            elif 52 <= i <= 54 or 80 <= i <= 82 or 108 <= i <= 110:
+            elif 52 <= i <= 54 or 80 <= i <= 82 or 108 <= i <= 110:  # ninth number
                 num_9.append(_)
             i += 1
     return final_account
 
 
-def printing_details(account_nr: str):
+def printing_details(account_nr: tuple):
     """Simple function needed to print all data about account in one step"""
 
     statement = 'Data from scanner: {}\nData status: {}\nOther possibilities: {}'.format(
@@ -193,7 +191,15 @@ def printing_details(account_nr: str):
     print(statement)
 
 
-def check_possible_numbers(account_to_check, valid_list_of_numbers=None):
+def check_possible_numbers(account_to_check: tuple, valid_list_of_numbers=None):
+    """Function for checking number which was invalid
+
+    Arguments:
+        account_to_check (tuple): str of an account number and list of characters taken from invalid number (if it was)
+
+    Returns:
+        possible (list): list of other possibilities based on invalid character
+    """
     if valid_list_of_numbers is None:
         valid_list_of_numbers = num_val_list
     possible = []
@@ -220,7 +226,7 @@ example = '''\
 |_||_||_||_||_||_||_||_||_|
 |_||_||_||_||_||_||_||_||_|'''
 
-
 if __name__ == '__main__':
+
     account = lines_on_numbers(example)
     printing_details(account)
