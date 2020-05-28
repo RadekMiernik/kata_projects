a188da1d0a3dbb99ec8929341bb42020faf1d96b
Radek Miernik <radoslaw.miernik@gmail.com>
2020-05-28 19:56:16 +0200

    working_with_proper_statement_solution

--- args/solution.py
+++ args/solution.py
@@ -1,60 +1,29 @@
 # this task is from https://codingdojo.org/kata/Args/
 
-# def test_parse_params():
-#     log_flag = FlagDefinition(name="-l", type=bool)
-#     port_flag = FlagDefinition(name="-p", type=int)
-#     dst_flag = FlagDefinition(name="-d", type=str)
-#     flags = [log_flag, port_flag, dst_flag]
-#
-#     result = parse(input="-l -p 8080 -d /usr/logs", flags=flags)
-
-
-class Flag:
-
-    def __init__(self, flag: str, expected_type):
-        self.flag = flag
-        self.expected_type = expected_type
-
-    def result(self, statement):
-        pass
-#
-
-#
-# def returning_values():
-#     statement = '-l'  # input('Please input your command line:\n')
-#     error_msg = 'Sorry but your command is wrong'                     # TODO: those variables should be at the end
-#
-#     flags = {'-l': ['LOG', False, None], '-p': ['PORT', 0, None], '-d': ['DIR', '', None],
-#              '-g': ['LIST OF STR', [], None], '-m': ['LIST OF INT', [], None]}
-#     used_flags = []
-#     for _ in flags.keys():
-#         if _ in statement:
-#             flags[_][2] = statement.index(_)
-#             used_flags.append(_)
-#     for _ in used_flags:
-#         if len(used_flags) == 1:
-#             print('{}: {}'.format(flags[_][0], flags[_][1]))
-#     print(len(used_flags))
-#     print(flags)
-#     print(used_flags)
-#     print(flags['-g'][2] != None)
-#
-#     log = flags['-l'][1]
-#     port = flags['-p'][1]
-#     directory = flags['-d'][1]
-#     list_of_str = flags['-g'][1]
-#     list_of_int = flags['-m'][1]
-#     final_statement = 'Log: {}\nPort: {}\nDirectory: {}\nList of str: {}\nList of int: {}'.format(
-#         log, port, directory, list_of_str, list_of_int)
-#
-#     # or
-#     print('**' * 20)
-#     for _ in flags.keys():
-#         print('{}: {}'.format(flags[_][0], flags[_][1]))
-#     print('**' * 20)
-#
-#     # return final_statement
-#
-#
-#
-# print(returning_values())
+command = '-l -p 8080 -d /usr/logs'
+list_of_data = command.split(' ')
+log_info = False
+port_info = 0
+dir_info = ''
+print(list_of_data)
+try:
+    if '-l' in list_of_data:
+        log_info = True
+        if not list_of_data[(list_of_data.index('-l') + 1)].startswith('-'):
+            print('You added unnecessary value \'{}\' after -l: we ignored it.'.format(
+                list_of_data[(list_of_data.index('-l') + 1)]))
+    if '-p' in list_of_data:
+        port_info = list_of_data[(list_of_data.index('-p') + 1)]
+        try:
+            port_info = int(port_info)
+        except ValueError:
+            port_info = 'Port can be only a number'
+    if '-d' in list_of_data:
+        dir_info = list_of_data[(list_of_data.index('-d') + 1)]
+        if len(dir_info) == 0:
+            dir_info = 'You passed too many spaces between -d and the path'
+except ValueError:
+    print('Remember about proper syntax')
+print(log_info)
+print(port_info)
+print(dir_info)
